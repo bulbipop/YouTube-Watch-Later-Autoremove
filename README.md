775018b63a7fa034d866d9a34ee616315a159608
bulbipop <fierbenj@gmail.com>
2017-11-05 17:33:55 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 youtube-WL-autoremove
+
+Autoremove video from Watch Later playlist when middleclicked.
---
+++ main.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Youtube-Watch-Later-AutoRemove
+// @namespace    https://github.com/bulbipop/youtube-WL-autoremove
+// @version      0.1
+// @match        https://www.youtube.com/playlist?list=WL
+// @run-at       document-idle
+// ==/UserScript==
+
+function clickDeleteButton() {
+    document.querySelector('ytd-menu-service-item-renderer').click()
+}
+
+function main() {
+    document.addEventListener("mousedown", function(ev) {
+        if (ev.button == 1) {
+            console.log('clicked something')
+            if (ev.target.className == 'style-scope ytd-playlist-video-renderer') {
+                console.log('it\'s a video!')
+                console.log(ev.target)
+                ev.target.closest('ytd-playlist-video-renderer').querySelector('button').click()
+                setTimeout(clickDeleteButton, 1000)
+            }
+        }
+    })
+}
+
+setTimeout(main, 2000)
